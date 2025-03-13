d7785730651b42e38d20e4b5e1fbbdbebb40b87b
Ashan Ekanayake <ashanekanayakeat@gmail.com>
2025-03-13 12:43:13 +0530

    add favicon

--- index.php
+++ index.php
@@ -4,7 +4,8 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>CricketAcademy</title>
+    <link rel="icon" type="image/png" href="images/faviconcricket.png" sizes="48x48">
     <link rel="stylesheet" href="styles.css">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
 </head>
@@ -37,7 +38,22 @@
             </div>
         </div>
     </nav>
-
+    <h1>Hello</h1>
+    <h1>Hello</h1>
+    <h1>Hello</h1>
+    <h1>Hello</h1>
+    <h1>Hello</h1>
+    <h1>Hello</h1>
+    <h1>Hello</h1>
+    <h1>Hello</h1>
+    <h1>Hello</h1>
+    <h1>Hello</h1>
+    <h1>Hello</h1>
+    <h1>Hello</h1>
+    <h1>Hello</h1>
+    <h1>Hello</h1>
+    <h1>Hello</h1>
+    <h1>Hello</h1>
     <footer class="footer text-light py-4" style="background-color: #001F3F;">
     <div class="container">
         <div class="row">
